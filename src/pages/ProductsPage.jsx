50b2f0b5ab8707ecf42542476285ae1ed4125eae
Goose <207717976+Gus-gallery@users.noreply.github.com>
2025-10-29 20:45:00 +0100

    Link to order page

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import NavBar from '../components/NavBar'
-import ProductCard from '../components/ProductCard'
 import ProductCarousel from '../components/ProductCard'
 
 const ProductsPage = () => {
@@ -8,7 +8,13 @@ const ProductsPage = () => {
     <main>
         <NavBar />
         <div className="flex items-center justify-center text-secondary font-regular text-base mt-6">
-          All orders by inquiry.
+          All orders by
+          <Link 
+            to="/orders"
+            className="text-secondary underline hover:text-primary transition-colors ml-1"
+          >
+            inquiry
+          </Link>.
         </div>
         <ProductCarousel />
     </main>
