66c2536275019329ccf3517963691142c042b562
Goose <207717976+Gus-gallery@users.noreply.github.com>
2025-11-03 17:35:05 +0100

    base with slash

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "gwl-der_site",
+  base: "/gwl-der_site/",
   plugins: [react(), tailwindcss()],
 })
